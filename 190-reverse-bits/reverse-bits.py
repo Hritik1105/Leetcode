6a650878bc21638ff97c35c771a9145194547b92
Hritik Kumar <hritik1105@gmail.com>
2026-02-16 09:43:08 +0530

    Time: 40 ms (74.42%) | Memory: 19.3 MB (48.27%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    #Hritik
+    def reverseBits(self, n: int) -> int:
+        if n==0: return 0
+        ans=0
+        while n>0:
+            ans+=1<<(32-(n&-n).bit_length())
+            n&=(n-1)
+        return ans
+        
